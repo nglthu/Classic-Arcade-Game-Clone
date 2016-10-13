a47668f54649ac9d308cb8b7bbd05c12ee5dd767
nglthu <nglthu@gmail.com>
2016-10-13 15:39:36 +1300

    App: Comment

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Declare all using variables 
 var enemyX1 = Math.random();
 var enemyY1 = Math.random() + 80;
 var enemyX2 = Math.random() - 100;
@@ -22,41 +23,38 @@ var playerPositionMaxY = 388;
 var playerPositionMinX = 0;
 var playerPositionMinY = 0.5;
 var playerX = 200;
-var playerY = 320;
+var playerY = 330;
 var playerImage = 'images/char-boy.png';
 var canvasWidthMin = -100;
 var score = 10;
 document.getElementById('score').value = score;
-var movement = 50;
+var positionReachPointY = 20;
+var movement = 100;
 var gemImage = 'images/GemOrange.png';
 var heart = 'images/Heart.png';
 var gemX = Math.random() + 300;
 var gemY = 50;
-var positionReachPointY = 20;
+//Declare super class of Contestant that Enemy and Player can inherit
 var Contestant = function (image, x, y, speed) {
 	this.image = image;
 	this.x = x;
 	this.y = y;
 	this.speed = speed;
 };
+//Declare and implement method for Contestant class
 Contestant.prototype.render = function () {
 	ctx.drawImage(Resources.get(this.image), this.x, this.y);
 };
-// Enemies our player must avoid
+//Declare enemy class that inherit form Contestant
 var Enemy = function (x, y, speed) {
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
 	Contestant.call(this, enemyImage, x, y, speed);
 };
+//Declare that Enemy's prototype can inherit form Contestant's method.
 Enemy.prototype = Object.create(Contestant.prototype);
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+//Update score and where enemy collised with player, losing score.
 Enemy.prototype.update = function (dt) {
-	// You should multiply any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
 	//this.x = this.x + this.speed * dt;
 	if (this.x < canvas.height) {
 		this.x += (this.speed * dt);
@@ -64,7 +62,8 @@ Enemy.prototype.update = function (dt) {
 	else {
 		this.x = canvasWidthMin;
 	}
-	//this.y = this.y + this.speed * dt;
+	//when enemy close to player
+	//score decrease
 	if (this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 50 && this.y + 40 > player.y) {
 		if (score > 0) {
 			score--;
@@ -73,30 +72,32 @@ Enemy.prototype.update = function (dt) {
 			score = score;
 		}
 		document.getElementById('score').value = score;
-		player.reset();
+		player.reset(playerX,playerY);
 	}
 };
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Declare Player inherit from Contestant Class
 var Player = function () {
 	Contestant.call(this, playerImage, playerX, playerY, 0);
 };
+//Player inherite method of Contestant
 Player.prototype = Object.create(Contestant.prototype);
+//Declare and implement method for Player
 Player.prototype.update = function () {
 	if (player.y < positionReachPointY) {
 		score++;
 		document.getElementById('score').value = score;
-		this.reset();
+		this.reset(playerX,playerY);
 		console.log('score:' + score);
 	}
+	//player at position of gem 
+	//score increase
 	if ((player.y < gemY + 10) && (player.y = gemY) && (player.x > gemX - 30) && (player.x < gemX + 30)) {
 		score++;
 		document.getElementById('score').value = score;
-		//this.reset();
 		console.log('score gem:' + score);
 	}
 };
+//Handle input moving left, right, up, down with movement position
 Player.prototype.handleInput = function (direction) {
 	if (direction == 'left' && this.x > playerPositionMinX) {
 		this.x -= movement;
@@ -111,17 +112,12 @@ Player.prototype.handleInput = function (direction) {
 		this.y += movement;
 	}
 };
-Player.prototype.reset = function () {
-	this.x = 200;
-	this.y = 340;
+Player.prototype.reset = function (x,y) {
+	this.x = x;
+	this.y = y;
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-//var allEnemies = [new Enemy()];
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//instance enemy
+//All enemies have their own random positions and speeds running the screen.
 var enemy1 = new Enemy(enemyX1, enemyY1, enemySpeedX1);
 var enemy2 = new Enemy(enemyX2, enemyY2, enemySpeedX2);
 var enemy3 = new Enemy(enemyX3, enemyY3, enemySpeedX3);
@@ -129,9 +125,10 @@ var enemy4 = new Enemy(enemyX4, enemyY4, enemySpeedX4);
 var enemy5 = new Enemy(enemyX5, enemyY5, enemySpeedX5);
 var enemy6 = new Enemy(enemyX6, enemyY6, enemySpeedX6);
 var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
-
+//new instance object of Player
 var player = new Player();
-
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. 
 document.addEventListener('keyup', function (e) {
 	var allowedKeys = {
 		37: 'left'
@@ -145,7 +142,7 @@ var Gem = function (x, y) {
 	this.x = x;
 	this.y = y;
 };
-
+//Gem when player is Gem, blood is filled
 Gem.prototype.render = function () {
 	if ((player.y < gem.y + 10) && (player.y = gem.y) && (player.x > gem.x - 30) && (player.x < gem.x + 30)) {
 		ctx.drawImage(Resources.get(heart), this.x, this.y);
@@ -155,5 +152,5 @@ Gem.prototype.render = function () {
 		ctx.drawImage(Resources.get(gemImage), this.x, this.y);
 	}
 };
-
+//new instance object of Gem
 var gem = new Gem(gemX, gemY);
